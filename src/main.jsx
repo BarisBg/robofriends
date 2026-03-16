48464c77ff2fefb6b43a9f6d543ef5fd50e6901c
Barış <barisethem@outlook.com>
2026-03-16 15:57:00 +0300

    inital commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Card from './components/Card.jsx';
+
+import App from './containers/App.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
